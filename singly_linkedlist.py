36cc72938979e366bf6367787c3a83cda8200064
Brianoksoftware <79844711+Brianoksoftware@users.noreply.github.com>
2023-07-01 23:47:20 +0300

    Added the file singly_linkedlist.py to the repo
    Python 3 code implementation of a singly linked list.

---
+++ singly_linkedlist.py
@@ -0,0 +1,42 @@
+#Linked list data structure(singly linked list)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.link = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+    
+    def insertFront(self, data):
+        newnode = Node(data)
+        newnode.link = self.head
+        self.head = newnode
+    
+    def insertEnd(self, data):
+        newnod = Node(data)
+        if self.head == None:
+            self.head = newnod
+        else:
+            current = self.head
+            while current.link != None:
+                current = current.link
+            current.link = newnod
+    
+    def printOut(self):
+        if self.head == None:
+            print("Linked list empty!")
+        else:
+            current = self.head
+            while current != None:
+                print(current.data, end="->")
+                current = current.link
+            print("none")
+
+llobj = Linkedlist()
+
+llobj.insertEnd(10)
+llobj.insertEnd(20)
+llobj.insertEnd(30)
+llobj.insertFront(9)
+llobj.printOut() 
