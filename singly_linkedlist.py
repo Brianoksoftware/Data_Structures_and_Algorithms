c0204e09d8c03f9bab71da19ffa13772d6b3ac93
Brianoksoftware <79844711+Brianoksoftware@users.noreply.github.com>
2023-07-03 23:59:38 +0300

    Update singly_linkedlist.py
    added another linked list implementation.

--- singly_linkedlist.py
+++ singly_linkedlist.py
@@ -39,4 +39,55 @@ llobj.insertEnd(10)
 llobj.insertEnd(20)
 llobj.insertEnd(30)
 llobj.insertFront(9)
+
+
+#Singly linked list
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.link = None
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+        
+    def insertStart(self, data):
+        newnode = Node(data)
+        newnode.link = self.head
+        self.head = newnode
+
+    def printOut(self):
+        if self.head == None:
+            print("Linked list empty!)
+        else:
+            current = self.head
+            while current != None:
+                print(current.data, end="->")
+            
+        
+        
+        
+        
+        
+        
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 llobj.printOut() 
