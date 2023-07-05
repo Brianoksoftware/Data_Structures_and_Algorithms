558f09a658e673480e817587525034816714f7d4
Brianoksoftware <79844711+Brianoksoftware@users.noreply.github.com>
2023-07-05 14:41:24 +0300

    Update singly_linkedlist.py
    Final singly linked list data structure EXAMPLE with 
    methods to insert nodes at front & end of Linked list

--- singly_linkedlist.py
+++ singly_linkedlist.py
@@ -88,6 +88,55 @@ llobj.insertEnd(7)
 llobj.insertEnd(9)
 llobj.printOut()
 
+'''
+Final singly linked list data structure EXAMPLE with 
+methods to insert nodes at front & end of Linked list
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.link = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def insertMwanzo(self,data): #insertFront
+        nodempya = Node(data)
+        nodempya.link = self.head
+        self.head = nodempya
+
+    def insertMwisho(self, data): #insertEnd
+        nodemanyien = Node(data)
+        current = self.head
+        while current.link != None:
+            current = current.link
+        current.link = nodemanyien
+
+    def printer(self):
+        if self.head == None:
+            print("Linked list is EMPTY!")
+        else:
+            current = self.head
+            while current != None:
+                print(current.data, end="->")
+                current = current.link
+            print("None")
+
+llistobj = Linkedlist()
+llistobj.printer()
+
+llistobj.insertMwanzo(4)
+llistobj.insertMwanzo(3)
+llistobj.insertMwanzo(1)
+llistobj.printer()
+
+llistobj.insertMwisho(6)
+llistobj.insertMwisho(8)
+llistobj.insertMwisho(10)
+llistobj.printer()
+
+
 
             
             
