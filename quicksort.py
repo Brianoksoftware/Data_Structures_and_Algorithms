3fcd3fa26b65eb0e7b75c48738f9e5cde384a4b5
Brianoksoftware <79844711+Brianoksoftware@users.noreply.github.com>
2023-07-08 18:26:41 +0300

    updated quicksort.py
    Final implementation of quicksort algorithm

--- quicksort.py
+++ quicksort.py
@@ -14,3 +14,23 @@ def quick_sort(arr):
 				greater.append(arr[i])
 		return quick_sort(lesser) + [pivot] + quick_sort(greater)
 print("Quicksorted:", quick_sort([5,2,3,1,4,0]))
+
+#Quicksort algorithm implemented recursively...FINAL IMPLEMENTATION
+def quicksoort(arr):
+	if len(arr) <= 1:
+		return arr 
+	else:
+		pivot = arr[len(arr)//2]
+		lesser = []
+		greater = []
+
+		for i in arr:
+			if arr[i] < pivot:
+				lesser.append(arr[i])
+			elif arr[i] > pivot:
+				greater.append(arr[i])
+		return quicksoort(lesser) + [pivot] + quicksoort(greater)
+print("ALMOST FINAL QUICKSORT:", quicksoort([5,3,1,4,2,0]))
+
+
+
